b2576ff51a4eb4baa6a408458c6a3b219129c148
dev-Taimoor7 <taimoorsajjad980@gmail.com>
2023-09-11 19:14:47 +0500

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './components/signin/signin.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddCourseComponent } from './components/courses/add-course/add-course.component';
+import { CourseListComponent } from './components/courses/course-list/course-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: DashboardComponent
+  },
+  {
+    path: 'signin', component: SigninComponent
+  },
+  {
+    path: 'courses', component: CourseListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { AddCourseComponent } from './components/courses/add-course/add-course.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CourseListComponent } from './components/courses/course-list/course-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    DashboardComponent,
+    SidenavComponent,
+    AddCourseComponent,
+    HeaderComponent,
+    CourseListComponent,
   ],
   imports: [
     BrowserModule,
