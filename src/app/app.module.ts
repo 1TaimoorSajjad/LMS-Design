832e389b040f9f01288adcd8e46351cf10a05b1c
“rao-officials” <rao.officials@gmail.com>
2023-10-05 19:11:52 +0500

    remove extra components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,21 +17,14 @@ import { AddCategoriesComponent } from './components/categories/add-categories/a
 import { SubCategoriesListComponent } from './components/sub-categories/sub-categories-list/sub-categories-list.component';
 import { AddSubCategoriesComponent } from './components/sub-categories/add-sub-categories/add-sub-categories.component';
 import { DocumentsComponent } from './components/instructors/documents/documents.component';
-import { Course1Component } from './components/courses/course1/course1.component';
-import { Course2Component } from './components/courses/course2/course2.component';
-import { Course3Component } from './components/courses/course3/course3.component';
 import { Course4Component } from './components/courses/course4/course4.component';
 import { Course5Component } from './components/courses/course5/course5.component';
-import { Course6Component } from './components/courses/course6/course6.component';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { getStorage } from 'firebase/storage';
 import { ManageCourseComponent } from './components/courses/manage-course/manage-course.component';
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -49,12 +42,8 @@ import { ManageCourseComponent } from './components/courses/manage-course/manage
     SubCategoriesListComponent,
     AddSubCategoriesComponent,
     DocumentsComponent,
-    Course1Component,
-    Course2Component,
-    Course3Component,
     Course4Component,
     Course5Component,
-    Course6Component,
     ManageCourseComponent,
 
   ],
